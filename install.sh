9a1651e75abf93743f534ed20581905e69eafdbd
chag <chag@users.noreply.github.com>
2025-09-26 22:13:06 -0700

    permissions

--- install.sh
+++ install.sh
@@ -14,10 +14,11 @@ chmod u+rx,go-wx /usr/local/bin/fan.sh
 
 echo "copying fan.service to /lib/systemd/system/"
 cp --force ./fan.service /lib/systemd/system/
-chmod 644 /lib/systemd/system/fan.service
+chmod go-w /lib/systemd/system/fan.service
 
 echo "copying fan.json to /etc/ (unless it already exists)"
 cp --no-clobber ./fan.json /etc/
+chmod go-w /etc/fan.json
 
 echo "reloading systemctl daemon"
 systemctl daemon-reload
